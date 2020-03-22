ef559d46ec75df964c74e87bdf606a48eb75a4fb
Nastya <nastya_mi@icloud.com>
2020-03-22 19:04:06 +0300

    feat: typical-arr-problems is done

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 
 exports.min = function min (array) {
-  return 0;
+    if (Array.isArray(array) === false || array === undefined) return 0;
+    else if (array.length === 0) return 0;
+
+    return array.reduce((a, b) => Math.min(a, b));
 }
 
 exports.max = function max (array) {
-  return 0;
+    if (Array.isArray(array) === false || array === undefined) return 0;
+    else if (array.length === 0) return 0;
+
+    return array.reduce((a, b) => Math.max(a, b));
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if (Array.isArray(array) === false || array === undefined) return 0;
+    else if (array.length === 0) return 0;
+
+    return array.reduce((a, b) => a + b, 0) / array.length;
 }
